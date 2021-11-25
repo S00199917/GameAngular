c8a7b61395885037cc3306bbc24c1c7385f53201
Jack Malone <S00199917@mail.itsligo.ie>
2021-11-25 21:30:10 +0000

    Added games form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameDetailsComponent } from './Games/game-details/game-details.component';
+import { GameFormComponent } from './Games/game-form/game-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameDetailsComponent
+    GameDetailsComponent,
+    GameFormComponent
   ],
   imports: [
     BrowserModule,
