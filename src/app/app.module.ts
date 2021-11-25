7484f144df777b96569a8f270ffb9922c4c87c05
Jack Malone <S00199917@mail.itsligo.ie>
2021-11-25 21:28:01 +0000

    removed games component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GamesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
