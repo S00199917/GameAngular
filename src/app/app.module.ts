82aef8fc164b844e27230dca2585c94bef7bb709
Jack Malone <S00199917@mail.itsligo.ie>
2021-11-25 21:31:27 +0000

    Added game row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GameDetailsComponent } from './Games/game-details/game-details.component';
 import { GameFormComponent } from './Games/game-form/game-form.component';
 import { GameListComponent } from './Games/game-list/game-list.component';
+import { GameRowComponent } from './Games/game-row/game-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameDetailsComponent,
     GameFormComponent,
-    GameListComponent
+    GameListComponent,
+    GameRowComponent
   ],
   imports: [
     BrowserModule,
