3846d0a789880bab59f77a1fcbc4f1c7be6c58d0
Jack Malone <S00199917@mail.itsligo.ie>
2021-11-25 21:29:40 +0000

    Added games details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameDetailsComponent } from './Games/game-details/game-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameDetailsComponent
   ],
   imports: [
     BrowserModule,
